319750fdef9504d7ff9fe2b5ec4badae552f4a41
lukhor <lukhor@gmail.com>
2013-10-30 18:43:25 +0100

    carousel

--- application/views/header_bootstrap.php
+++ application/views/header_bootstrap.php
@@ -6,10 +6,12 @@
     <title>bootstrap</title>
     <link rel="stylesheet" type="text/css" href="application/views/css/bootstrap.css">
 </head>
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script src="application/views/js/bootstrap.js"></script>
+
+<script>$('.carousel').carousel()$('.carousel').carousel()</script>
 
 <body>
-    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src=”application/views/js/bootstrap.js”></script>
 
 <div class="container">
     <h1><a href="”#”">Bootstrap Site</a></h1>
@@ -37,6 +39,44 @@
 
     <a href="#" class="btn btn-large btn-success">Get Started</a>
 </div>
+
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <!--<ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>-->
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="http://i.haymarket.net.au/Galleries/20121213024803_BMW-M6-Gran-Coupe-1[2]%20(1).jpg" alt="img1">
+                <div class="carousel-caption">
+                   <p>img1</p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="http://stwot.motortrend.com/files/2013/04/BMW-6-Series-Gran-Coupe-Uwe-Duttmann-model-in-front-of-theater-close-crop.jpg" alt="img2">
+                <div class="carousel-caption">
+                    <p>img2</p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="http://i.haymarket.net.au/Galleries/20121213024803_BMW-M6-Gran-Coupe-1[2]%20(1).jpg" alt="img3">
+                <div class="carousel-caption">
+                    <p>img3</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>    </div>
 
 <div class="row">
     <div class="span3">
